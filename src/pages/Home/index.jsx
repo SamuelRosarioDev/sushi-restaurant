1cc86a8dd266fefcc75d3d6826231636e699df52
SamuelRosarioDev <samuelrosario.dev@gmail.com>
2024-09-15 15:29:22 -0300

    add menu 0.1

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,101 @@
-import backgroundSushi from '../../assets/img/backgroundSushi.jpg'
-import photoSushi from '../../assets/img/sushiPhoto.jpg'
+import backgroundSushi from "../../assets/img/backgroundSushi.jpg";
+import photoSushi from "../../assets/img/sushiPhoto.jpg";
 
-import Header from '../../components/Header';
-import { Container, Backgroud, HeaderLinks, Presentation } from "./style";
+import iconSushi from "../../assets/icons/sushi.png";
+import iconRamen from "../../assets/icons/ramen.png";
+import iconTemaki from "../../assets/icons/temaki.png";
+import iconBarcaDeSushi from "../../assets/icons/barcadesushi.png";
+
+import imgSushiDeSalmao from '../../assets/listJaponeseFood/sushiDeSalmao.png'
+
+import Header from "../../components/Header";
+import {
+	Container,
+	Backgroud,
+	HeaderLinks,
+	Presentation,
+	Menu,
+	JapaneseFood,
+	FoodCard,
+    FoodInfos,
+} from "./style";
 
 export default function Home() {
-    return (
-        <Container>
-            <Backgroud>
-            <img className='backgroundImg' src={backgroundSushi} alt="background sushi" />
-            <Header>
-                <h3>Restautante Sushi</h3>
-                <HeaderLinks>
-                    <a href="#d">Agendar</a>
-                    <a href="#d">Cardápio</a>
-                </HeaderLinks>
-            </Header>
-            <Presentation>
-                <span>
-                    <h2>Experimente a culinária japonesa</h2>
-                    <p>Diversas refeições com tematicas japonesas e uma variedade gigante de sabores a descobrir!</p>
-                </span>
-                <img src={photoSushi} alt="foto de sushi" />
-            </Presentation>
-
-            </Backgroud>   
-
-        </Container>
-    );
+	return (
+		<Container>
+			<Backgroud>
+				<img
+					className="backgroundImg"
+					src={backgroundSushi}
+					alt="background sushi"
+				/>
+				<Header>
+					<h3>Restautante Sushi</h3>
+					<HeaderLinks>
+						<a href="#d">Agendar</a>
+						<a href="#menu">Cardápio</a>
+					</HeaderLinks>
+				</Header>
+				<Presentation>
+					<span>
+						<h2>Experimente a culinária japonesa</h2>
+						<p>
+							Diversas refeições com tematicas japonesas e uma variedade gigante
+							de sabores a descobrir!
+						</p>
+					</span>
+					<img src={photoSushi} alt="foto de sushi" />
+				</Presentation>
+			</Backgroud>
+			<Menu id="menu">
+				<ul>
+					<li>
+						<img src={iconSushi} alt="icon sushi" />
+						<p>Sushi</p>
+					</li>
+					<li>
+						<img src={iconRamen} alt="icon ramen" />
+						<p>Ramen</p>
+					</li>
+					<li>
+						<img src={iconTemaki} alt="icon temaki" />
+						<p>Temaki</p>
+					</li>
+					<li>
+						<img src={iconBarcaDeSushi} alt="icon barca de sushi" />
+						<p>Combos</p>
+					</li>
+				</ul>
+				<JapaneseFood>
+					<FoodCard>
+						<img src={imgSushiDeSalmao} alt="sushi de salmão" />
+						<FoodInfos>
+                            <span>Disponivel</span>
+							<p>Sushi de salmão 9 pec</p>
+                            <span>R$30,00</span>
+						</FoodInfos>
+                        <a href="#agendar">Agendar</a>
+					</FoodCard>
+                    <FoodCard>
+						<img src={imgSushiDeSalmao} alt="sushi de salmão" />
+						<FoodInfos>
+                            <span>Disponivel</span>
+							<p>Sushi de salmão 9 pec</p>
+                            <span>R$30,00</span>
+						</FoodInfos>
+                        <a href="#agendar">Agendar</a>
+					</FoodCard>
+                    <FoodCard>
+						<img src={imgSushiDeSalmao} alt="sushi de salmão" />
+						<FoodInfos>
+                            <span>Disponivel</span>
+							<p>Sushi de salmão 9 pec</p>
+                            <span>R$30,00</span>
+						</FoodInfos>
+                        <a href="#agendar">Agendar</a>
+					</FoodCard>
+				</JapaneseFood>
+			</Menu>
+		</Container>
+	);
 }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -7,9 +7,10 @@ export const Container = styled.section`
 export const Backgroud = styled.div`
    position: relative;
     height: 100vh;
-    overflow-x: hidden;
     background-color: #000;
+    border-radius: 0 0 1rem 1rem;
     z-index: 0;
+    box-shadow: 0 0 5rem 0.5rem #111111;
     .backgroundImg {
         position: absolute;
         z-index: -1;
@@ -20,6 +21,7 @@ export const Backgroud = styled.div`
         object-fit: cover;
         opacity: 0.4;
     }
+
 `;
 
 export const HeaderLinks = styled.div`
@@ -27,9 +29,28 @@ export const HeaderLinks = styled.div`
     display: flex;
     gap: 1rem; 
     a{
+        position: relative;
         text-decoration: none;
         font-weight: bold;
         font-size: clamp(1rem, 3vw, 24px);
+        &::after{
+            content: "";
+            background-color: #fff;
+            border-radius: 1rem;
+            width: 3px;
+            height: 3px;
+            position: absolute; 
+            bottom: -3px;
+            left: 0;
+            transition: 1s ease-in-out;
+
+        }
+        &:hover{
+            &::after{
+                width: 100%;
+                transition: 0.5s ease-in-out;
+            }
+        }
     }
 
     
@@ -64,11 +85,103 @@ export const Presentation = styled.div`
 
         span{
             h2{
-                font-size: 5vw;
+                font-size: clamp(1.5rem, 3vw, 24px);
             }
             p{
-                font-size: 4vw;
+                font-size: clamp(1rem, 3vw, 24px);
             }
         }
+    }
+`
+
+export const Menu = styled.div`
+    height: 100vh;
+    width: 100%;
+
+    ul{
+        background-color: #e3e3e3;
+
+        border-left: 12rem;
+        list-style-type: none;
+
+
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        li{
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+            padding: 1rem ;
+            img{
+                width: clamp(2.5rem, 4vw, 15rem);
+            }
+            p{
+                font-size: clamp(1rem, 3vw, 1.5rem);
+                color: #000;
+            }
+        }
+    }
+
+`
+
+export const JapaneseFood = styled.div` 
+    box-shadow: 0 0 5rem 0.5rem #111111;
+    background-color: #111111;
+    border-radius:1rem 1rem 0 0 ;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    padding: 1rem;
+    flex-wrap: wrap;
+`
+
+export const FoodCard = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    z-index: 1;
+    background-color: #333333;
+    border-radius: 1rem;
+    padding: 1rem;
+    gap: 1rem;
+
+    img{
+    
+        width: clamp(100px, 20vw, 500px);
+        border-radius: 1rem;
+        z-index: -1;
+    }
+
+    a{
+        text-align: center;
+        background-color: #008000;
+        padding: 1rem;
+        text-decoration: none;
+        border-radius: 1rem;
+        font-weight: bold;
+    }
+    @media (min-width: 1000px){
+        flex-direction: column;
+        align-items: flex-start;
+        a{
+            padding: 0.8rem;
+            width: 100%;
+        }
+    }
+
+`
+
+export const FoodInfos = styled.div`
+    p{
+        color: #fff;
+    }
+
+    span{
+        color: #00FF00;
     }
 `
